25594345e86a26b18ab6b4c012de2bc1e4262793
slowlife7 <slowlife7@kakao.com>
2021-12-03 17:38:29 +0900

    git pull 테스트
    git pull 테스트

--- src/main/java/next/controller/CreateUserController.java
+++ src/main/java/next/controller/CreateUserController.java
@@ -17,7 +17,7 @@ public class CreateUserController implements Controller {
     public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
         User user = new User(req.getParameter("userId"), req.getParameter("password"), req.getParameter("name"),
                 req.getParameter("email"));
-        log.debug("User : {}", user);
+        log.debug("User : {}!!", user);
 
         DataBase.addUser(user);
         return "redirect:/";
